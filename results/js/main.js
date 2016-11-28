1bed0c703f262da36e393fcd9617f86cff913c22
Corbin Muraro <corbinmuraro@gmail.com>
2016-11-28 22:36:20 +0000

    Update responsiveness, copywriting

--- results/js/main.js
+++ results/js/main.js
@@ -6,29 +6,32 @@ q = q.replace(/\+/g,' ');
 
 $('#search-field').val(q);
 
-$.getJSON("http://ec2-52-41-3-172.us-west-2.compute.amazonaws.com:8900/solr/core1/select/?q=" + q + "&wt=json&json.wrf=?&indent=true", function(result){
-  $('.summary').append(summaryTemplate({totalresults: result.response.numFound, query: q}));
 
-  for (var i = 0; i < result.response.docs.length; i++) {
-    $(".results").append(resultTemplate({url: result.response.docs[i].url, title: result.response.docs[i].user, text: result.response.docs[i].text}));
-    $(".results").append();
-  }
+$.ajax({url:'http://ec2-52-41-3-172.us-west-2.compute.amazonaws.com:8900/solr/core1/select/',
+	dataType: 'jsonp',
+	data: buildSearchParams(q), 
+	traditional: true,
+	jsonp: 'json.wrf',
+	success: function(result) {
+		$('.summary').append(summaryTemplate({totalresults: result.response.numFound, query: q}));
+
+		for (var i = 0; i < result.response.docs.length; i++) {
+    		  $(".results").append(resultTemplate({url: result.response.docs[i].url, title: result.response.docs[i].user, text: result.response.docs[i].text}));
+  		}
+	}
 });
 
-function buildSearchParams(q, offset) {
+function buildSearchParams(q) {
   var ret = { 
-  'rows': HITSPERPAGE,
-  'wt': 'json',
-  'q': q,
-  'start': offset
+    'rows': 50,
+    'wt': 'json',
+    'q': q,
+    'hl': 'true',
+    'hl.fl': 'text',
+    'hl.simple.pre': '<strong>',
+    'hl.simple.post': '</strong>',
+    'hl.snippets': 10
   };
-  if (HL_FL) {
-    ret['hl'] = 'true';
-    ret['hl.fl'] = HL_FL;
-    ret['hl.simple.pre'] = HL_SIMPLE_PRE;
-    ret['hl.simple.post'] = HL_SIMPLE_POST;
-    ret['hl.snippets'] = HL_SNIPPETS;
-  }
   return ret;
 }
 
