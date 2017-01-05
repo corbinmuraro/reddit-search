139b4579d658914c5f883ac38c98f79fa7db4afe
Corbin Muraro <corbinmuraro@gmail.com>
2017-01-05 23:08:55 +0000

    Add more helpful 0 results page with link to reddit search; JS TODOs

--- results/js/main.js
+++ results/js/main.js
@@ -19,34 +19,47 @@ $(function() {
     	jsonp: 'json.wrf',
     	success: function(result) {
             var numResults = result.response.numFound;
-            $('.summary').append(summaryTemplate({totalresults: numResults, query: q}));
 
-            for (var i = 0; i < result.response.docs.length; i++) {
-                var id = result.response.docs[i].id;
-                var highlighted_text = result.highlighting[id].text[0];
-                
-                var clean_post = clean_up_post_urls(highlighted_text);
-
-                // ?context=10 to show at most 10 parent comments up the chain
-                $(".results").append(resultTemplate({url: result.response.docs[i].url + "?context=10", title: result.response.docs[i].user, text: clean_post}));
-            }
-
-            if (numResults < RESULTS_PER_PAGE) {
+            if (numResults === 0)
+            {   
+                var search_on_reddit = "https://www.reddit.com/r/malefashionadvice/search?q=" + qs.substring(2, qs.lastIndexOf("&")) + "&restrict_sr=on&sort=relevance&t=all";
+                $('.summary').append("<div class='no-results-head'>I couldn't find anything under <strong>"+ q + "</strong>.</div>");
+                $('.summary').append("<div class='no-results-link'><a href='" + search_on_reddit + "'>Search for <strong>" + q + "</strong> in thread titles on reddit.com/r/malefashionadvice</a></div>");
                 $('.nav').hide();
+                $('.summary').css({'margin-bottom': '0'});
             }
-            else if (p < RESULTS_PER_PAGE) {
-                $('.first-nav').show();
-            }
-            else if ((numResults - p ) <= 50) {
-                $('.last-nav').show();
-            }
+
             else {
-                $('.middle-nav').show();
-                var pagNum_number = Math.floor(p / RESULTS_PER_PAGE) + 1;
-                var pagNum_string = '' + pagNum_number;
-                $('.page-number .number').text(pagNum_string);
-                if (pagNum_number === 2)
-                    $('.prev-prev-page').hide();
+                $('.summary').append(summaryTemplate({totalresults: numResults, query: q}));
+
+                for (var i = 0; i < result.response.docs.length; i++) {
+                    var id = result.response.docs[i].id;
+                    var highlighted_text = result.highlighting[id].text[0];
+                    
+                    var clean_post = clean_up_post_urls(highlighted_text);
+
+                    // ?context=10 to show at most 10 parent comments up the chain
+                    $(".results").append(resultTemplate({url: result.response.docs[i].url + "?context=10", title: result.response.docs[i].user, text: clean_post}));
+                }
+
+                navView();
+                if (numResults < RESULTS_PER_PAGE) {
+                    $('.nav').hide();
+                }
+                else if (p < RESULTS_PER_PAGE) {
+                    $('.first-nav').show();
+                }
+                else if ((numResults - p ) <= RESULTS_PER_PAGE) {
+                    $('.last-nav').show();
+                }
+                else {
+                    $('.middle-nav').show();
+                    var pagNum_number = Math.floor(p / RESULTS_PER_PAGE) + 1;
+                    var pagNum_string = '' + pagNum_number;
+                    $('.page-number .number').text(pagNum_string);
+                    if (pagNum_number === 2)
+                        $('.prev-prev-page').hide();
+                }
             }
     	},
         error: function(jq,status,message) {
@@ -59,7 +72,6 @@ $(function() {
         }
     });
 });
-
 
 // return key = search button click
 $("#search-field").keyup(function(event){
@@ -82,27 +94,29 @@ $(".search-button").click(function(){
   }
 });
 
+// loads next page of results
 $(".next-page").click(function() {
     var params = {q:q, p:p+RESULTS_PER_PAGE};
     var queryString = jQuery.param( params );
     window.location = "http://sqs.corbinmuraro.com/results/?" + queryString;
 });
 
+// loads previous page of results
 $(".prev-page").click(function(){
     var params = {q:q, p:p-RESULTS_PER_PAGE};
     var queryString = jQuery.param( params );
     window.location = "http://sqs.corbinmuraro.com/results/?" + queryString;
 });
 
+
+// jumps back to page 1
 $(".prev-prev-page").click(function(){
     var params = {q:q, p:0};
     var queryString = jQuery.param( params );
     window.location = "http://sqs.corbinmuraro.com/results/?" + queryString;
 });
 
-
-
-// builds the solr search URL
+// builds the solr search URL for AJAX
 function buildSearchParams(q, p) {
     var ret = { 
         'start': p,
@@ -119,23 +133,33 @@ function buildSearchParams(q, p) {
     return ret;
 }
 
+
 var bad_url_regex = /(https?:\/\/)(www\.)?(.*?)?(<strong>)(.*?)(<\/strong>)/g;
 var good_url_regex = /(?!.*?")(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=;]*))/g;
 var reddit_url_regex = /\[(.*?)\]\s?\((.*?)\)/g;
 
+// scrub solr's highlighting (<strong> and </strong>) off of the urls
+// turn text links into html links
+// returns the scrubbed a <a href>ed post text
 function clean_up_post_urls(post_text) {
-    // scrub <strong> and </strong> off of the urls
     while (bad_url_regex.test(post_text)) {
         post_text = post_text.replace(bad_url_regex, '$1$2$3$5');
     }
+    // ^ TODO: fix regex to NOT target "Just go [full](http://i.imgur.com/KSQEdce.jpg) [<strong>kanye</strong>](http://i.imgur.com/MBZQf4H.jpg) "
+    // (http://i.imgur.com/KSQEdce.jpg) <-> [<strong>kanye</strong>] getting paired, resulting in scrubbing of <strong></strong> from []
+
     var clean_text = post_text;
 
+    // add links to urls in the form of []()
     var with_reddit_urls = clean_text.replace(reddit_url_regex, '<a href="$2">$1</a>');
     
+    // add links to plaintext urls
     var with_regular_urls = with_reddit_urls.replace(good_url_regex,'<a href="$1">$1</a>');
 
     return with_regular_urls;
 }
+
+
 
 // takes the page's query string as a parameter
 // stores q (query) and p (page # in posts) values into the q and p global variables
